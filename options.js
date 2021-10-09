d9a87a2d15b0f61a19d2ac8a5e598b0b1d40f698
Getsumi3 <7727826XG>
2021-10-09 17:36:09 +0300

    Replaced `var` with `const/let` in all files

--- options.js
+++ options.js
@@ -1,11 +1,11 @@
 module.exports = combineOptions;
 
-var normalizeColor = require('./lib/normalize-color.js');
+const normalizeColor = require('./lib/normalize-color.js');
 
 function combineOptions(options) {
   options = options || Object.create(null);
 
-  var clearColor = normalizeColor(options.clearColor);
+  const clearColor = normalizeColor(options.clearColor);
 
   /**
    * Background of the scene in hexadecimal form. Default value is 0x000000 (black);
