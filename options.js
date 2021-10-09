7cbaacd5170c3231644d23d2901609210dcee7fa
Getsumi3 <7727826XG>
2021-10-09 17:36:48 +0300

    tabs and spaces

--- options.js
+++ options.js
@@ -3,14 +3,14 @@ module.exports = combineOptions;
 const normalizeColor = require('./lib/normalize-color.js');
 
 function combineOptions(options) {
-  options = options || Object.create(null);
+    options = options || Object.create(null);
 
-  const clearColor = normalizeColor(options.clearColor);
+    const clearColor = normalizeColor(options.clearColor);
 
-  /**
-   * Background of the scene in hexadecimal form. Default value is 0x000000 (black);
-   */
-  options.clearColor = typeof clearColor === 'number' ? clearColor : 0x000000;
+    /**
+     * Background of the scene in hexadecimal form. Default value is 0x000000 (black);
+     */
+    options.clearColor = typeof clearColor === 'number' ? clearColor : 0x000000;
 
-  return options;
+    return options;
 }
